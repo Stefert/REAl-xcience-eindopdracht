5da1d3b15d481a9f7ba509aa2dedbf4ab70c10ea
Stefert <138987654+Stefert@users.noreply.github.com>
2024-10-23 07:36:48 +0000

    random ah changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re 
+import random
 
 # Dictionary voor patronen.
 patterns = { 
@@ -8,9 +9,13 @@ patterns = {
 
 # Dictionary voor veelvoorkomende berichten.
 responses = {
-  "Hello": ["Hi, how are you?", "Hello!"],
-  "I am fine": ["Good to hear!", "Awesome!"]
-}
+  "hello": ["Hi, how are you?", "Hello!"],
+  "i am fine": ["Good to hear!", "Awesome!"],
+  "how about you": ["im  fine, thanks!", "im okay, thanks for asking"],
+  "i feel sad" : ["im sorry to hear that, how can i assist you?" , "if you feel down you can always seek help from proffesionals"],
+  "i need help" : ["how can i help you" , "okay, what can i do for you?"],
+  "i feel great" : ["Wel thats awazing to hear!"]
+   }
 
 # Functie die het antwoord van de chatbot teruggeeft.
 # Dit antwoord komt uit de patterns dictionary,
@@ -24,8 +29,7 @@ def get_response(message):
      if match:
        # hint: patterns[pattern].xxx( ... ) waar xxx een python-functie is.
        # hint: lees hoofdstuk 4.2
-       return "Hier komt het antwoord van de chatbot uit de patterns dictionary" 
-  
+       return pattern + match.group(1)
   # Zoek een antwoord in de 'responses' dictionary
   # en return dit antwoord als het is gevonden.
   # Als het niet is gevonden, dan zegt de bot jou na.
@@ -33,12 +37,13 @@ def get_response(message):
     # hint: lees hoofdstuk 2.2 en hoofdstuk 3.3
     return random.choice(responses[message])
   else:
-    return "I don't understand you."
+    return "i heard you, you said: " + message 
 
 while True:
   message = input("YOU: ")
   response = get_response(message)
   print("Bot: " + response)
+
 
 
 
